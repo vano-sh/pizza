6afd393d3d36505c10a9f9d9a91b475734fbf95d
Shiryaev Ivan <vano-sh@mail.ru>
2023-06-29 09:50:34 +0300

    Other code...

--- src/components/layout/DisplayCards/_DisplayCards.js
+++ src/components/layout/DisplayCards/_DisplayCards.js
@@ -3,6 +3,7 @@ import { Card } from './components/Card'
 export const DisplayCards = () => {
   const className = 'display-cards'
 
+  // Other code...
   return (
     <section className={className}>
       <h1 className={`${className}__title`}>Все пиццы</h1>
