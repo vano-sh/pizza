42136309b48853cec5d7f5550ff27284ee2cd314
Shiryaev Ivan <vano-sh@mail.ru>
2023-06-29 10:08:30 +0300

    Code...

--- src/components/layout/DisplayCards/_DisplayCards.js
+++ src/components/layout/DisplayCards/_DisplayCards.js
@@ -4,6 +4,8 @@ export const DisplayCards = () => {
   const className = 'display-cards'
 
   // Other code...
+  // Code... 
+
   return (
     <section className={className}>
       <h1 className={`${className}__title`}>Все пиццы</h1>
